fafbec8d570e04b2080edc46347d2b0386c26567
neerajp99 <neerajp1999@gmail.com>
2019-06-17 23:15:25 +0530

    Edit tests for test class PluginTest

--- Unit/PluginTest.php
+++ Unit/PluginTest.php
@@ -17,38 +17,25 @@ use Drupal\Tests\UnitTestCase;
 class PluginTest extends UnitTestCase {
 
   /**
-   * Define __construct function.
-   */
-  public function __construct() {
-    parent::__construct();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setUp() {
-    parent::setUp();
-  }
-
-  /**
    * Tests for class NetworkManager.
    */
   public function testNetworkManager() {
     $namespaces = $this->createMock(Traversable::class);
     $cache_backend = $this->createMock(CacheBackendInterface::class);
     $module_handler = $this->createMock(ModuleHandlerInterface::class);
+
     $networkManager = $this->getMockBuilder(NetworkManager::class)
-      ->setConstructorArgs(array($namespaces, $cache_backend, $module_handler))
+      ->setConstructorArgs([$namespaces, $cache_backend, $module_handler])
       ->getMock();
-    parent::__construct();
-    $this->assertTrue($networkManager instanceof NetworkManager);
+
     $this->assertTrue(
      method_exists($networkManager, 'setCacheBackend'),
-     'NetworkManager does not implements setCacheBackend function/method'
+     'NetworkManager class does not implements setCacheBackend function/method'
     );
+
     $this->assertTrue(
      method_exists($networkManager, 'alterInfo'),
-     'NetworkManager does not implements alterInfo function/method'
+     'NetworkManager class does not implements alterInfo function/method'
     );
   }
 
@@ -57,13 +44,15 @@ class PluginTest extends UnitTestCase {
    */
   public function testNetworkInterface() {
     $networkInterface = $this->createMock(NetworkInterface::class);
+
     $this->assertTrue(
       method_exists($networkInterface, 'authenticate'),
-      'NetworkManager does not implements authenticate function/method'
+      'NetworkManagerInterface class does not implements authenticate function/method'
     );
+
     $this->assertTrue(
       method_exists($networkInterface, 'getSdk'),
-      'NetworkManager does not implements getSdk function/method'
+      'NetworkManagerInterface class does not implements getSdk function/method'
     );
   }
 
@@ -74,33 +63,37 @@ class PluginTest extends UnitTestCase {
     $entity_type_manager = $this->createMock(EntityTypeManagerInterface::class);
     $config_factory = $this->createMock(ConfigFactoryInterface::class);
     $container = $this->createMock(ContainerInterface::class);
-    $configuration = array();
-    $plugin_definition = array();
+    $configuration = [];
+    $plugin_definition = [];
 
     $networkBase = $this->getMockBuilder('Drupal\social_api\Plugin\NetworkBase')
-      ->setConstructorArgs(array($configuration,
+      ->setConstructorArgs([$configuration,
         'drupal123',
         $plugin_definition,
         $entity_type_manager,
         $config_factory,
-      ))
+      ])
       ->setMethods(['getSdk', 'create'])
       ->getMockForAbstractClass();
+
     $this->assertTrue(
       method_exists($networkBase, 'init'),
-      'NetworkBase does not implements init function/method'
+      'NetworkBase class does not implements init function/method'
     );
+
     $this->assertTrue(
       method_exists($networkBase, 'create'),
-      'NetworkBase does not implements create function/method'
+      'NetworkBase class does not implements create function/method'
     );
+
     $this->assertTrue(
       method_exists($networkBase, 'authenticate'),
-      'NetworkBase does not implements authenticate function/method'
+      'NetworkBase class does not implements authenticate function/method'
     );
+
     $this->assertTrue(
       method_exists($networkBase, 'getSdk'),
-      'NetworkBase does not implements getSdk function/method'
+      'NetworkBase class does not implements getSdk function/method'
     );
   }
 
