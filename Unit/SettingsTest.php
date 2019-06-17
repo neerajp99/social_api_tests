ca698a8a499174b6c6e7e65a0bdb820a807a8cab
neerajp99 <neerajp1999@gmail.com>
2019-06-17 23:15:40 +0530

    Edit tests for Settings

--- Unit/SettingsTest.php
+++ Unit/SettingsTest.php
@@ -14,20 +14,6 @@ use Drupal\Tests\UnitTestCase;
 class SettingsTest extends UnitTestCase {
 
   /**
-   * Define __construct function.
-   */
-  public function __construct() {
-    parent::__construct();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setUp() {
-    parent::setUp();
-  }
-
-  /**
    * Tests for class Settings.
    */
   public function testSettingsBase() {
@@ -38,27 +24,29 @@ class SettingsTest extends UnitTestCase {
     $storage = $this->createMock(StorageInterface::class);
     $event_dispatcher = $this->createMock(EventDispatcherInterface::class);
     $typed_config = $this->createMock(TypedConfigManagerInterface::class);
+
     $configs = $this->getMockBuilder('Drupal\Core\Config\ImmutableConfig')
-      ->setConstructorArgs(array($config,
+      ->setConstructorArgs([$config,
         $storage,
         $event_dispatcher,
         $typed_config,
-      ))
+      ])
       ->getMock();
 
     $settingsBase = $this->getMockBuilder(SettingsBase::class)
-      ->setConstructorArgs(array($configs))
+      ->setConstructorArgs([$configs])
       ->getMockForAbstractClass();
 
     $this->assertTrue(
       method_exists($settingsBase, 'getConfig'),
       'SettingsBase does not implements getConfig function/method'
     );
+
     $this->assertTrue(
       method_exists($settingsBase, 'factory'),
       'SettingsBase does not implements factory function/method'
     );
-    $settingsBase->getConfig();
+    // $settingsBase->getConfig();
   }
 
 }
