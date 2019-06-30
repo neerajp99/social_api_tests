e55aa868589ab7afe5bbf60b57a250a91440df7b
neerajp99 <neerajp1999@gmail.com>
2019-06-29 19:54:15 -0700

    Modified test for class  SocialApiDataHandler

--- Unit/SocialApiDataHandlerTest.php
+++ Unit/SocialApiDataHandlerTest.php
@@ -12,28 +12,50 @@ use Drupal\Tests\UnitTestCase;
 class SocialApiDataHandlerTest extends UnitTestCase {
 
   /**
+   * Interface for the session.
+   *
+   * @var \Symfony\Component\HttpFoundation\Session\SessionInterface
+   */
+  protected $session;
+
+  /**
+   * Variables are written to and read from session via this class.
+   *
+   * @var \Drupal\social_api\SocialApiDataHandler
+   */
+  protected $socialApiDataHandler;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->session = $this->getMock(SessionInterface::class);
+
+    $this->socialApiDataHandler = $this->getMockBuilder(SocialApiDataHandler::class)
+      ->setConstructorArgs([$this->session])
+      ->setMethods(NULL)
+      ->getMockForAbstractClass();
+  }
+
+  /**
    * Tests for class SocialApiDataHandler.
    */
   public function testSocialApiDataHandler() {
     $key = "drupal";
     $value = "drupal123";
-    $session = $this->getMock(SessionInterface::class);
 
-    $socialApiDataHandler = $this->getMockBuilder(SocialApiDataHandler::class)
-      ->setConstructorArgs([$session])
-      ->setMethods(['get'])
-      ->getMockForAbstractClass();
+    $this->socialApiDataHandler->set($key, $value);
 
-    $socialApiDataHandler->set($key, $value);
+    $this->socialApiDataHandler->setSessionPrefix('1234');
 
-    $socialApiDataHandler->setSessionPrefix('1234');
+    $this->session->expects($this->any())
+      ->method('get')
+      ->willReturn($this->socialApiDataHandler->getSessionPrefix() . $key);
 
-    $socialApiDataHandler->method('get')
-      ->with($key)
-      ->willReturn($session->get($socialApiDataHandler->getSessionPrefix() . $key));
-
-    $this->assertEquals('1234_', $socialApiDataHandler->getSessionPrefix());
-    $this->assertEquals($session->get($socialApiDataHandler->getSessionPrefix() . $key), $socialApiDataHandler->get($key));
+    $this->assertEquals('1234_', $this->socialApiDataHandler->getSessionPrefix());
+    $this->assertEquals('1234_drupal', $this->socialApiDataHandler->get($key));
   }
 
 }
